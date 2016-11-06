25884bf88a66e18bb5348df2fab3e5ef8afd59c4
kpeterr <kpeterr2@gmail.com>
2016-11-06 13:36:10 +0100

    Függvények és névterek

--- fuggvenyek2.php
+++ fuggvenyek2.php
@@ -39,7 +39,9 @@
 
             /*
             function server() {
-                var_dump($_SERVER);
+                echo "<pre>";
+                print_r($_SERVER);
+                echo "</pre>";
             }
             server();
             */
@@ -49,7 +51,9 @@
             echo "Visszatérési értékek<br>";
             function ret_teszt() {
                 // amit a "return" után írunk, azza fog a függvényünk visszatérni
-                return "Hello World!<br>"; 
+                return "Hello World!<br>";
+                // ha a return után próbáluk meg valait futtatni az már nem fog lefutni,
+                // mert a return megállítja a függvény futását
             }
             // az echo függvénnyel liíratjuk a visszatérési értékét a függvényünknek
             echo ret_teszt();
@@ -58,6 +62,45 @@
             $ezt_kaptuk_vissza = ret_teszt(); // értéket adunk a változónak
             echo $ezt_kaptuk_vissza;
 
+            echo "<hr>";
+            // Tetszőleges számú argumentum (paraméternek vagy adatnak is hívják)
+            echo "Tetszőleges számú argumentum:<br>";
+            function arg_teszt() { // a zárójelek közé kerül(nek) az argumentum(ok)
+                // ezek is változók, de csak a függvényen belül láthatók
+                // az alábbi beépített függvény visszaadja az összes argumentumot,
+                // amit a függvénynek átadunk és ezt elmentjük egy változóba ($args)
+                $args = func_get_args(); 
+                echo "<pre>";
+                print_r($args); // majd kiíratjuk a végeredményt
+                echo "</pre>";
+            }
+            arg_teszt('szia', 'hello', 'szevasztok');
+
+            /*
+            * Amennyiben megadtunk egy argumentumot,
+            * de a függvény meghívásánál nem adunk meg paramétert,
+            * mindenképpen adjunk neki értéket ($adat=1),
+            * mert ha nem adunk neki, akkor hibát jelez a PHP
+            function arg_teszt1( $adat = 1) {
+                $args = func_get_args(); 
+                echo "<pre>";
+                print_r($args); // majd kiíratjuk a végeredményt
+                echo "</pre>";
+            }
+            arg_teszt1(); // Egy üres tömböt ad vissza, ha nem adunk meg neki értéket
+            */
+
+            echo "<br>";
+
+            echo "Tetszőleges számú argumentumból csak egy használata:<br>";
+            function arg_teszt2() { 
+                $args = func_get_arg(2); // az 2-es kulcsú elemet kérjük használni
+                echo "<pre>";
+                print_r($args);
+                echo "</pre>";
+            }
+            // a 2-es kulcsú elemet írja csak ki (szevasztok)
+            arg_teszt2('szia', 'hello', 'szevasztok');
         ?>
     </body>
 </html>
