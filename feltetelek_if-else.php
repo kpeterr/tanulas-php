53594b7a3cbe1cc96936825b80c4dd9b9ab83069
kpeterr <kpeterr2@gmail.com>
2016-11-12 23:58:11 +0100

    Feltételek létrehozása

---
+++ feltetelek_if-else.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=utf-8 />
+        <title>Feltételek (conditions) - if-else</title>
+    </head>
+    <body>
+        <?php
+            
+        ?>
+    </body>
+</html>
