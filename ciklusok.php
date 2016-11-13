fe448b996de14b227e85a36f5f9820bf4cb6c242
kpeterr <kpeterr2@gmail.com>
2016-11-13 12:12:12 +0100

    Ciklusok (loop) létrehozása

---
+++ ciklusok.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=utf-8 />
+        <title>Ciklusok (loop)</title>
+    </head>
+    <body>
+        <?php
+            echo "<h1>Ciklusok (loop)</h1>";
+
+        ?>
+    </body>
+</html>
