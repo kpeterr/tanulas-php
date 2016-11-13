6641577bc6b463616e19a734549b87894ed3268e
kpeterr <kpeterr2@gmail.com>
2016-11-13 15:23:41 +0100

    Fájlok beszúrása (include-require)

---
+++ fajlok-beszurasa.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=utf-8 />
+        <title>Fájlok beszúrása (include-require)</title>
+    </head>
+    <body>
+        <?php
+            echo "<h1>Fájlok beszúrása (include-require)</h1>";
+
+            
+        ?>
+    </body>
+</html>
