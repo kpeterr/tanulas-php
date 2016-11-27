b4ba6644453917fdb1e34e471b7c7f809c499a05
kpeterr <kpeterr2@gmail.com>
2016-11-27 12:48:39 +0100

    Szuperglobális változók kezelése -

--- szuperglobalis_valtozok.php
+++ szuperglobalis_valtozok.php
@@ -2,38 +2,40 @@
 <html>
     <head>
         <meta charset=utf-8 />
-        <title>Változók</title>
+        <title>Szuperglobális változók</title>
     </head>
     <body>
+
+        <h1>Szuperglobális változók</h1>
+
+        <form>
+            <input type="text" name="nev" placeholder="Név">
+            <br>
+            <input type="text" name="kor" placeholder="Életkor">
+            <br>
+            <input type="text" name="munka" placeholder="Munka">
+            <br>
+            <input type="submit" value="Küldés">
+        </form>
+
+        <br>
+
         <?php
-            echo "<h1>Változó típusok</h1>";
 
-            // BOOLEAN típusú változó
-            echo "<strong>BOOLEAN típusú változó:</strong><br>";
-            $true = true;       // egy boolean típusú változó felvétele
-            echo $true."<br>";  // a változó kiírása, a kimenete "1" lesz
-            $false = false;     // egy boolean típusú változó felvétele
-            echo $false;        // a változó kiírása, a kimenete "semmi" lesz, mert a "0"-át nem jeleníti meg
+            /*
+            * Olyan változók, amelyek a PHP-n belül bárhonnan elérhetőek.
+            * Beépített változók, amelyeket nem lehet módosítani!
+            */
 
-            echo "<br>";
+            // Adatok átadása egyik oldalról a másikra
+            // $_GET (query string - kérdő szöveg) globális változóval
+            // Ez egy tömb, amely tartalmazza azokat az értékeket,
+            // melyeket az URL-ben kapott meg az oldal
+            // var_dump($_GET);
 
-            // INTEGER típusú változó
-            echo "<strong>INTEGER típusú változó:</strong><br>";
-            $a = 56;
-            $b = 125;
-            echo $a + $b ."<br>";
-
-            echo "<br>";
-
-            // STRING típusú változó
-            echo "<strong>STRING típusú változó:</strong><br>";
-            $str = 'Ez egy szöveg';         // Így nem értelmezi az idézőjelek közötti részt
-            $str = $str.', kibővítve';
-            $s1 = 'kibővített';
-            $str1 = "Ez egy $s1 szöveg";    // Így értelmezi az idézőjelek közötti részt
-            echo $str."<br>";
-            echo $str1;
-            echo "";
+            echo "<strong>A változó alkalmazása:</strong><br>";
+            if (isset($_GET['nev'])) // Ezzel megvizsgáljuk, hogy létezik-e az adott változó (nev)
+                echo "<p>Hello ".$_GET['nev']."!</p><br>";
         ?>
     </body>
 </html>
